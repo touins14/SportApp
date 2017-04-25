f3dc61df342944faaf5ded1182a4ffbd87145337
haythem oueslati <haythem oueslati>
2017-04-25 11:08:17 +0200

    standby touranement work 1/2

--- src/application/organiser/touranement/ManageStandByTouranement.js
+++ src/application/organiser/touranement/ManageStandByTouranement.js
@@ -51,6 +51,13 @@ export default class ManageStandByTouranement extends React.Component {
 
     this.state = {
       activeTab: '',
+      numbercompetetion:'',
+      ar:[],
+      mytab:[],
+      mytabrefused:[],
+      mytabstandby:[],
+      tk:'',
+      dodo:'1'
 
     };
 
@@ -60,7 +67,11 @@ export default class ManageStandByTouranement extends React.Component {
 
   handleActiveState(eventKey) {
     this.setState({
-      activeTab: eventKey
+      activeTab: eventKey,
+      mytab:[],
+      mytabrefused:[],
+      mytabstandby:[]
+
     });
   }
 
@@ -78,17 +89,19 @@ export default class ManageStandByTouranement extends React.Component {
 //*************************
 componentDidMount() {
   var tk=localStorage.getItem('token');
+this.setState({tk:localStorage.getItem('token')})
 
 
-
-  axios.get('http://sportapp.astrolabs.io/api/allTeams'+'?token='+tk
+  axios.get('http://sportapp.astrolabs.io/api/competitionConstruction'+'?token='+tk
         )
         .then((response) => {
           console.log('haw chnoua jé',response);
   //this.setState({ar: response.data})
-   
-           
+   this.setState({numbercompetetion: response.data.numbersCompetitionConstruction})
+           this.setState({ar:response.data.competitionConstruction})
+console.log('piww',this.state.numbercompetetion);
 
+console.log('ahawa el tableau',this.state.ar);
 
   //console.log('fsfds',this.state.ar);
 
@@ -97,12 +110,84 @@ componentDidMount() {
           console.log(error);
         });
   //console.log('fsfds',this.state.ar);
+  //*******************************************************TEam accepted
+
 }
 //*****************
+nombretab()
+{
+  var length=this.state.numbercompetetion
+  console.log('haw menna',this.state.numbercompetetion);
 
+
+}
+getaccepted(id)
+{
+console.log('mytabpane',this.state.activeTab);
+
+var tk=this.state.tk
+if (id=this.state.activeTab)
+{axios.get('http://sportapp.astrolabs.io/api/competitionConstructionAccepted'+'?token='+tk+'&id='+id
+      )
+      .then((response) => {
+        console.log('haw chnoua jé',response.data[0].team_accepted);
+
+ this.setState({mytab:response.data[0].team_accepted})
+console.log('dazitou',this.state.mytab);
+
+//console.log('fsfds',this.state.ar);
+
+
+        }).catch((error) => {
+        console.log(error);
+      });}
+
+
+
+
+}
+getrefused(id)
+{
+  var tk=this.state.tk
+  if (id=this.state.activeTab)
+  {axios.get('http://sportapp.astrolabs.io/api/competitionConstructionRefused'+'?token='+tk+'&id='+id
+        )
+        .then((response) => {
+         console.log('haw chnoua jé',response.data[0].team_refused);
+
+   this.setState({mytabrefused:response.data[0].team_refused})
+  console.log('dazitou',this.state.mytabrefused);
+
+  //console.log('fsfds',this.state.ar);
+
+
+          }).catch((error) => {
+          console.log(error);
+        });}
+}
+getstandby(id)
+{var tk=this.state.tk
+if (id=this.state.activeTab)
+{axios.get('http://sportapp.astrolabs.io/api/competitionConstructionCurrent'+'?token='+tk+'&id='+id
+      )
+      .then((response) => {
+       console.log('haw chnoua jé',response.data[0].team_current);
+
+
+
+   this.setState({mytabstandby:response.data[0].team_current})
+  console.log('dazitou',this.state.team_current);
+
+//console.log('fsfds',this.state.ar);
+
+
+        }).catch((error) => {
+        console.log(error);
+      });}
+}
   render() {
     return (
-      <PanelTabContainer id='tabs-basic' defaultActiveKey="Champion1" style={{marginTop:25}}>
+      <PanelTabContainer id='tabs-basic' defaultActiveKey="0" style={{marginTop:25}}>
         <PanelHeader className='bg-lightblue fg-white'>
           <Grid>
             <Row>
@@ -117,22 +202,36 @@ componentDidMount() {
             <Row>
               <Col sm={12}>
                 <Nav bsStyle="tabs" onSelect={::this.handleActiveState} className='tab-lightblue'>
-                  <NavItem eventKey="Champion1">Champion1</NavItem>
-                  <NavItem eventKey="Champion2">Champion2</NavItem>
-                  <NavItem eventKey="Champion3">Champion3</NavItem>
+                {  this.state.ar.map(comp =>
+                  <NavItem eventKey={comp.id} key={comp.id} > {comp.name}</NavItem>
+                )}
+
+
+
+
                 </Nav>
                 <Tab.Content>
-                  <Tab.Pane eventKey="Champion1">
+                  {  this.state.ar.map(comp =>
+                  <Tab.Pane    eventKey={comp.id} key={comp.id}>
                   <Grid>
+
                   <Row>
                     <Col sm={4} collapseRight>
             <PanelContainer>
               <Panel>
+
                 <PanelBody>
+
                 <div className="list-group">
-  <a  className="list-group-item active">Team accepted</a>
-  <a href="#" className="list-group-item">Second item</a>
-  <a href="#" className="list-group-item">Third item</a>
+  <a  className="list-group-item active">Team accepted <Button xs outlined onClick={()=>{this.getaccepted(comp.id)}}  style={{marginBottom: 5}} bsStyle='success'>Show</Button></a>
+
+
+    {this.state.mytab.map(team =>
+
+      <a href="#" key={team.id} className="list-group-item">{team.name}</a>
+
+  )
+  }
 </div>
 
                 </PanelBody>
@@ -144,9 +243,13 @@ componentDidMount() {
               <Panel>
                 <PanelBody>
   <div className="list-group">
-  <a  className="list-group-item active">Team refused</a>
-  <a href="#" className="list-group-item">Second item</a>
-  <a href="#" className="list-group-item">Third item</a>
+  <a  className="list-group-item active">Team refused <Button xs outlined onClick={()=>{this.getrefused(comp.id)}}  style={{marginBottom: 5}} bsStyle='success'>Show</Button></a>
+    {this.state.mytabrefused.map(team =>
+
+      <a href="#" key={team.id} className="list-group-item">{team.name}</a>
+
+  )
+  }
 </div>
                 </PanelBody>
               </Panel>
@@ -157,9 +260,13 @@ componentDidMount() {
               <Panel>
                 <PanelBody>
  <div className="list-group">
-  <a  className="list-group-item active">Team in standby</a>
-  <a href="#" className="list-group-item">Second item</a>
-  <a href="#" className="list-group-item">Third item</a>
+  <a  className="list-group-item active">Team in standby  <Button xs outlined onClick={()=>{this.getstandby(comp.id)}}  style={{marginBottom: 5}} bsStyle='success'>Show</Button></a>
+    {this.state.mytabstandby.map(team =>
+
+      <a href="#" key={team.id} className="list-group-item">{team.name}</a>
+
+    )
+    }
 </div>
 <Button xs outlined  style={{marginBottom: 5}}   bsStyle='primary'>Contact Team</Button>{' '}
                 </PanelBody>
@@ -169,102 +276,7 @@ componentDidMount() {
         </Row>
         </Grid>
                   </Tab.Pane>
-                  <Tab.Pane eventKey="Champion2">
-                                        <Grid>
-                  <Row>
-                    <Col sm={4} collapseRight>
-            <PanelContainer>
-              <Panel>
-                <PanelBody>
-                <div className="list-group">
-  <a  className="list-group-item active">Team accepted</a>
-  <a href="#" className="list-group-item">Second item</a>
-  <a href="#" className="list-group-item">Third item</a>
-</div>
-
-                </PanelBody>
-              </Panel>
-            </PanelContainer>
-          </Col>
-          <Col sm={4} collapseRight>
-            <PanelContainer>
-              <Panel>
-                <PanelBody>
-  <div className="list-group">
-  <a  className="list-group-item active">Team refused</a>
-  <a href="#" className="list-group-item">Second item</a>
-  <a href="#" className="list-group-item">Third item</a>
-</div>
-                </PanelBody>
-              </Panel>
-            </PanelContainer>
-          </Col>
-          <Col sm={4}>
-            <PanelContainer>
-              <Panel>
-                <PanelBody>
- <div className="list-group">
-  <a  className="list-group-item active">Team in standby</a>
-  <a href="#" className="list-group-item">Second item</a>
-  <a href="#" className="list-group-item">Third item</a>
-</div>
-<Button xs outlined  style={{marginBottom: 5}}   bsStyle='primary'>Contact Team</Button>{' '}
-                </PanelBody>
-              </Panel>
-            </PanelContainer>
-          </Col>
-        </Row>
-        </Grid>
-
-
-                  </Tab.Pane>
-                <Tab.Pane eventKey="Champion3">
-                              <Grid>
-                  <Row>
-                    <Col sm={4} collapseRight>
-            <PanelContainer>
-              <Panel>
-                <PanelBody>
-   <div className="list-group">
-  <a  className="list-group-item active">Team accepted</a>
-  <a href="#" className="list-group-item">Second item</a>
-  <a href="#" className="list-group-item">Third item</a>
-</div>
-                </PanelBody>
-              </Panel>
-            </PanelContainer>
-          </Col>
-          <Col sm={4} collapseRight>
-            <PanelContainer>
-              <Panel>
-                <PanelBody>
-   <div className="list-group">
-  <a  className="list-group-item active">Team refused</a>
-  <a href="#" className="list-group-item">Second item</a>
-  <a href="#" className="list-group-item">Third item</a>
-</div>
-                </PanelBody>
-              </Panel>
-            </PanelContainer>
-          </Col>
-          <Col sm={4}>
-            <PanelContainer>
-              <Panel>
-                <PanelBody>
- <div className="list-group">
-  <a  className="list-group-item active">Team in standby</a>
-  <a href="#" className="list-group-item">Second item</a>
-  <a href="#" className="list-group-item">Third item</a>
-</div>
-<Button xs outlined  style={{marginBottom: 5}}   bsStyle='primary'>Contact Team</Button>{' '}
-                </PanelBody>
-              </Panel>
-            </PanelContainer>
-          </Col>
-        </Row>
-        </Grid>
-
-                  </Tab.Pane>
+                  )}
 
                 </Tab.Content>
               </Col>
